e2afe5a432d14e5f24cc5e68a7bb6b36b66c8619
omarzouaoui <zouaouiomar80@gmail.com>
2023-01-09 19:47:52 +0100

    Calculator buttons

--- calculator_app/lib/screens/home_screen.dart
+++ calculator_app/lib/screens/home_screen.dart
@@ -38,31 +38,109 @@ class _CalculatorState extends State<Calculator> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber,
+      backgroundColor: Colors.amberAccent,
       body: Column(
         children: [
           SizedBox(
-            height: MediaQuery.of(context).size.height/3,
+            height: MediaQuery.of(context).size.height/3.5,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
+
                 Container(
                   padding: const EdgeInsets.all(20.0),
                   alignment: Alignment.centerRight,
-                  child: const Text(
-                    "Input",
-                    style: TextStyle(
+                  child: Text(
+                    userInput,
+                    style: const TextStyle(
                       fontSize: 32,
                       color: Colors.white
                     ),
                   ),
-                )
+                ),
+
+                Container(
+                  padding: const EdgeInsets.all(20.0),
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    result,
+                    style: const TextStyle(
+                      fontSize: 48,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+
               ],
             ),
-          )
+          ),
+
+          const Divider(
+            color: Colors.white,
+          ),
+
+          Expanded(
+            child: Container(
+              // padding: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 20.0),
+              margin: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 20.0),
+              // EdgeInsets.fromLTRB(left, top, right, bottom)
+              child: GridView.builder(
+                itemCount: buttonList.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  crossAxisSpacing: 12,
+                  mainAxisSpacing: 12,
+                ), 
+                itemBuilder: (BuildContext context, int index){
+                  return CustomButton(buttonList[index]);
+                }
+                ),
+            )
+            )
+
         ]
       )
       ,
     );
   }
+
+  Widget CustomButton(String text){
+    return InkWell(
+      splashColor: Colors.amberAccent,
+      onTap: () {},
+      child: Ink(
+        decoration: BoxDecoration(
+          color: getBgColor(text),
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.white.withOpacity(0.1),
+              blurRadius: 4,
+              spreadRadius: 0.5,
+              offset: Offset(-3, -3),
+            )
+          ]
+        ),
+
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+              color: getBgColor(text),
+              fontSize: 30,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+        ),
+
+      ),
+    );
+  }
+
+  getBgColor(String text){
+    
+  }
+
 }
